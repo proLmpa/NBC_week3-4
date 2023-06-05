d9056d415a42f57f5322ba4c91d83b26b23c0371
heeyeoul kim <kheeyeoul@gmail.com>
2023-06-05 17:05:53 +0900

    add Memo(option 1) ~ print MemoList(option 2) completed

--- src/entity/MemoList.java
+++ src/entity/MemoList.java
@@ -1,67 +1,70 @@
 package entity;
 
+import java.sql.SQLOutput;
 import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
 
 public class MemoList {
-    private int count;
     private LinkedList<Memo> list;
+    private Scanner sc;
 
     public MemoList() {
-        count = 0;
         this.list = new LinkedList<>();
+        sc = new Scanner(System.in);
     }
 
-    // 메모 전체 조회 - Getter
-    public LinkedList<Memo> getMemoList() { return list; }
+    // 메모 리스트 전체 크기 반환
+    public int getSize() { return list.size(); }
 
-    // 메모 전체 조회 - Setter
-    public void setMemoList(LinkedList<Memo> memoList) {
-        this.list = memoList;
-    }
-
-    // 작성 최신순 메모 출력 (구현)
+    // 작성 최신순 메모 출력
     public void printMemoList() {
-        // 기존 list를 for문을 통해 접근해 출력하기
+        StringBuilder sb = new StringBuilder();
+        sb.append("---------------------- [ Memo List ] ---------------------\n");
+        sb.append("Post Number | Writer Name | Content         | Written Time\n");
+
+        // 작성 최신 순으로 메모를 출력한다.
+        ListIterator<Memo> it = list.listIterator(list.size());
+        while(it.hasPrevious()){
+            Memo memo = it.previous();
+            sb.append(String.format(" %10d | %11s | %-15s | %s\n", memo.getNum(), memo.getName(), memo.getPost(), memo.getTime()));
+        }
+
+        sb.append("----------------------------------------------------------\n");
+        System.out.print(sb);
     }
 
-    // 메모 1건 추가 (구현)
+    // 메모 1건 추가
     public void addMemo(Memo memo){
         // 기존 list에 Memo 추가
+        list.add(memo);
     }
 
-    // 글 수정/삭제 시, 글 번호를 받아서 해당 메모를 반환 (구현)
+    // 글 수정/삭제 시, 글 번호를 받아서 해당 메모를 반환
     public Memo getMemo(int idx) {
         // list에서 idx에 해당하는 Memo 반환하기
-        return null;
+        return list.get(idx);
     }
 
     // 메모 1건 수정 (구현)
     public void editMemo(int idx) {
-        // list에서 idx에 해당하는 Memo 가져오기
-        // 해당 Memo의 게시글(post) 필드 갱신
+        // (1) list에서 idx에 해당하는 Memo 가져오기
+        // (2) 해당 Memo 내용 출력하기
+
+        // (3) scanner로 수정할 내용 입력받기
+        // (4) Memo의 게시글(post) 필드 갱신
     }
 
     // 메모 1건 삭제 (구현)
     public void deleteMemo(int idx) {
-        // list에서 idx에 해당하는 Memo 가져오기
-        // list에서 해당 memo를 제거
-        // 글 삭제 후 글 번호 재지정 작업
+        // (1) list에서 idx에 해당하는 Memo 가져오기
+        // (2) list에서 해당 memo를 제거
 
-        // count 수정
-        minusCount();
+        editMemoNum(idx); // (3) 글 삭제 후 글 번호 재지정 작업
     }
 
     // 글 삭제 후 Memo의 num 필드 수정 (구현)
     public void editMemoNum(int idx){
-        return;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    // 글 삭제 후 count 값 수정
-    public void minusCount(){
-        count--;
+        // for 문으로 idx 부터 list.size()까지의 memo들의 num을 수정
     }
 }

914b903bbed44e60cded5b51355d856ef2114046
heeyeoul kim <kheeyeoul@gmail.com>
2023-06-05 16:57:36 +0900

    getter & setter completed

--- src/entity/Memo.java
+++ src/entity/Memo.java
@@ -17,6 +17,27 @@ public class Memo {
         time = dateFormat(Calendar.getInstance());
     }
 
+    public int getNum() { return num; }
+
+    public String getName() { return name; }
+
+    public String getPassword() { return password; }
+
+    public String getPost() { return post; }
+
+    public String getTime() { return time; }
+
+    // 글 수정 작업
+    public void setPost(String post) {
+        this.post = post;                           // post(내용) 변경
+        time = dateFormat(Calendar.getInstance());  // time(시간)을 새로 수정
+    }
+
+    // 글 삭제 후 글 번호 재지정
+    public void setNum(int num) {
+        this.num = num;
+    }
+
     // 작성일 형식 반환 함수
     public String dateFormat(Calendar today) {
         return today.get(Calendar.YEAR)+"-" + (today.get(Calendar.MONTH)+1)+"-"
@@ -24,9 +45,13 @@ public class Memo {
                 + today.get(Calendar.MINUTE)+":" + today.get(Calendar.SECOND);
     }
 
-    // 글 수정 작업 (구현)
-    public void editPost() {
-        // post(내용) 변경
-        // time(시간)을 새로 수정
+    public void printMemo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("-----------------\n");
+        sb.append(String.format("num: %s\n", num));
+        sb.append(String.format("name: %s\n", name));
+        sb.append(String.format("post: %s\n", post));
+        sb.append("-----------------\n");
+        System.out.print(sb);
     }
 }
